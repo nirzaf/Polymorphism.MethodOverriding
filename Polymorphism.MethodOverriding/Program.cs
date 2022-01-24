d0eb878be3f1471390c92b861b2b6bd36b2df90d
nirza <nirza@DESKTOP-A2VM3LC>
2022-01-24 22:11:15 +0530

    Add project files.

---
+++ Polymorphism.MethodOverriding/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+public class MobilePhone
+{
+    public virtual void Typing()
+    {
+        Console.WriteLine("Using Keypad");
+    }
+}
+
+public class SmartPhone : MobilePhone
+{
+    public override void Typing()
+    {
+        Console.WriteLine("Using QWERTY Keyboard");
+    }
+}
